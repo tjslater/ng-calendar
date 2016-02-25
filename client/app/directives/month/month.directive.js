8b882662ff4b8033a851dce303b27928717a43a0
James Slater <slaterj@google.com>
2016-02-25 09:13:15 -0800

    Moved the month generator to the datemanager service Removed dependency of month directive on the parsedevents object being passed down from parent directive

--- client/app/directives/month/month.directive.js
+++ client/app/directives/month/month.directive.js
@@ -1,30 +1,18 @@
 'use strict';
 
 angular.module('calendarConceptApp')
-  .directive('month', function (EventData) {
+  .directive('month', function (EventData, DateManager) {
     return {
       templateUrl: 'app/directives/month/month.html',
-      scope: {
-        events: "="
-      },
+      scope: {},
       restrict: 'EA',
       link: function (scope, element, attrs) {
 
-        var getDaysInMonth = function(month, year) {
-          var date = new Date(year, month, 1);
-          var days = [];
-          console.log('month', month, 'date.getMonth()', date.getMonth());
-          while (date.getMonth() === month) {
-            days.push(new Date(date));
-            date.setDate(date.getDate() + 1);
 
-          }
-          return days;
-        };
 
         scope.events = EventData.getParsedEvents(2012, 3);
 
-        scope.month = getDaysInMonth(2012, 3);
+        scope.month = DateManager.getDaysInMonth(2012, 3);
       }
     };
   });
--- client/app/services/datemanager/datemanager.service.js
+++ client/app/services/datemanager/datemanager.service.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('calendarConceptApp')
-  .service('datemanager', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('DateManager', function () {
+    this.getDaysInMonth = function(year, month) {
+      var date = new Date(year, month, 1);
+      var days = [];
+      console.log('month', month, 'date.getMonth()', date.getMonth());
+      while (date.getMonth() === month) {
+        days.push(new Date(date));
+        date.setDate(date.getDate() + 1);
+
+      }
+      return days;
+    };
   });
