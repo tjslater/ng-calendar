e9470250f17c1a30e03bb46b79d62b395bac77af
James Slater <slaterj@google.com>
2016-02-24 17:15:56 -0800

    Parsing of event objects to create a full calendar object using Date strings as keys to event arrays

--- client/app/services/eventdata/eventdata.service.js
+++ client/app/services/eventdata/eventdata.service.js
@@ -3,16 +3,136 @@
 angular.module('calendarConceptApp')
   .service('EventData', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
-    this.getEvents = function(){
+    var parsedEvents = {};
+    /**
+     * to get a length of an event, you turn the start and end days into milliseconds,
+     * subtract the startdate from the enddate,
+     * then divide by DAY_IN_MILLISECONDS to get the event length
+     * @const {number}
+     */
+    var DAY_IN_MILLISECONDS = 86400000;
+
+    /**
+     * Utility function primarily for benchmarking event parser
+     * @param name
+     * @returns {{stop: EventData.stop}}
+     */
+    var timer = function (name) {
+      var start = new Date();
+      return {
+        stop: function () {
+          var end = new Date();
+          var time = end.getTime() - start.getTime();
+          console.log('Timer:', name, 'finished in', time, 'ms');
+        }
+      }
+    };
+
+
+    /**
+     * Resolves on /calendar route in calendar.js
+     * Returns events object to Calendar controller
+     * @returns {HttpPromise}
+     */
+    this.getEvents = function () {
       return $http.get('/api/events')
     };
 
-    this.parseEvents = function(events) {
-      events.forEach(this.parseEvent)
+    /**
+     *
+     * @param events
+     */
+    this.parseEvents = function (events) {
+      var t = timer('event parser');
+      events.forEach(this.addEvent, this);
+      t.stop();
+    };
+    /**
+     *
+     * @param {number|string} year
+     * @param {number|string} month
+     * @returns {{Object []}}
+     */
+    this.getParsedEvents = function (year, month) {
+      var date, eventMonth, eventYear;
+      var monthEvents = {};
+      if(!month) {
+        /**
+         *
+         */
+        return parsedEvents;
+      }
+      for (var dateKey in parsedEvents) {
+        date = new Date(dateKey);
+        eventMonth = date.getMonth();
+        eventYear = date.getFullYear();
+
+        if(eventMonth === month && eventYear === year) {
+          monthEvents[dateKey] = parsedEvents[dateKey];
+        }
+      }
+      return monthEvents;
+
+
     };
 
-    this.parseEvent = function(event){
-      console.log(event);
+    /**
+     * Adds an event to the master event object
+     * Each key in the object is a key to an array of events for that date.
+     * If the startDate is equal to an endDate, add it by key
+     * If it's a multiday, iterate from its start date to its end date, adding it to each day in between
+     * @param event
+     * @param {number} idx
+     */
+    this.addEvent = function (event) {
+      if(!event) return;
+      var dateArray = event.startDate.split('-');
+      var startDate = new Date(dateArray[0], dateArray[1], dateArray[2]);
+      if(!parsedEvents[startDate]) {
+        parsedEvents[startDate] = [];
+      }
+      if(event.startDate === event.endDate) {
+        parsedEvents[startDate].push(event);
+      } else {
+        this.parseMultiDayEvent(event, startDate, true);
+      }
+    };
+    /**
+     * Multiday parser
+     * @param event
+     * @param startDate
+     * @param isFirstDay
+     */
+    this.parseMultiDayEvent = function (event, startDate, isFirstDay) {
+      var dateArray = event.endDate.split('-');
+      /**
+       * By splitting the string into an array, avoids locale time issue
+       * @type {Date}
+       */
+      var endDate = new Date(dateArray[0], dateArray[1], dateArray[2]);
+      var startIdx = null;
+      var eventLength = null;
+
+      while (startDate <= endDate) {
+        if(!parsedEvents[startDate]) parsedEvents[startDate] = [];
+        if(isFirstDay === false) event.startIdx = startIdx;
+        parsedEvents[startDate].push(event);
+        if(isFirstDay) {
+
+          //For multiday events, keep track of index value of firstDay
+          //May or may not be useful later on
+          startIdx = parsedEvents[startDate].indexOf(event);
+          /**
+           * For creating the multiday element in the view
+           * @type {number}
+           */
+          eventLength = Math.round(((endDate.getTime() - startDate.getTime()) / DAY_IN_MILLISECONDS)) + 1;
+          parsedEvents[startDate][startIdx].eventLength = eventLength;
+          parsedEvents[startDate][startIdx].startIdx = startIdx;
+          isFirstDay = false;
+        }
+        startDate.setDate(startDate.getDate() + 1)
+      }
     }
 
 
