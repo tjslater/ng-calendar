bdbeb33cadaa3c9219d43aa1ffc73a2a2cf6a2d9
Brian Sullivan <sullivanbrian@google.com>
2016-02-27 18:49:08 -0800

    Month directivebreaks days into weeks

--- client/app/directives/month/month.directive.js
+++ client/app/directives/month/month.directive.js
@@ -5,15 +5,27 @@ angular.module('calendarConceptApp')
     return {
       scope: {
         days: "=",
-        evts: "=",
-        week: "="
+        evts: "="
       },
       templateUrl: 'app/directives/month/month.html',
       restrict: 'EA',
       link: function (scope, element, attrs) {
-        // console.log(scope.days);
+        console.log(scope.days);
         // console.log(scope.evts);
+        scope.weeks = [];
 
+        function weekifyDays() {
+          var chunkSize = 7;
+          scope.weeks = scope.days.map( function(e,i){
+            return i % chunkSize === 0 ?
+                scope.days.slice(i, i + chunkSize) : null;
+          })
+          .filter(function(e){ return e; });
+        }
+
+        scope.$watch('days', function() {
+          weekifyDays();
+        });
       }
     };
   });
